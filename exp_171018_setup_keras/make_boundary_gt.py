213ce55236c2e3dfcb68574cdc6c05f54de030d4
Julian <julianhennies@hotmail.de>
2017-11-15 13:50:03 +0100

    storing the result

--- exp_171018_setup_keras/make_boundary_gt.py
+++ exp_171018_setup_keras/make_boundary_gt.py
@@ -2,11 +2,12 @@ import numpy as np
 import h5py
 from matplotlib import pyplot as plt
 
-data_filepath = '/media/julian/Daten/datasets/cremi_2016/cremi.splA.raw_neurons.testvolume.h5'
+data_filepath = '/media/julian/Daten/datasets/cremi_2016/cremi.splA.train.raw_neurons.crop.h5'
+data_targetpath = '/media/julian/Daten/datasets/cremi_2016/exp_171018_setup_keras/cremi.splA.train.boundaries.h5'
 
 with h5py.File(data_filepath, 'r') as f:
     raw = np.array(f['raw'])
-    labels = np.array(f['labels'])
+    labels = np.array(f['neuron_ids'])
 
 print(raw.shape)
 print(labels.shape)
@@ -17,3 +18,7 @@ boundaries = np.array([find_boundaries(labels[z]) for z in range(labels.shape[0]
 
 plt.imshow(boundaries[20, :, :])
 plt.show()
+
+with h5py.File(data_targetpath, 'w') as f:
+    f['data'] = boundaries
+
